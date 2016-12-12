3f8fa02c0125dbbb4f005cd154bc5b208c84acc8
David Cramer <david@digilab.co.za>
2016-12-12 12:15:47 +0200

    remove unused components

--- assets/controls/autocomplete/js/select2-init.js
+++
@@ -1,7 +0,0 @@
-( function( $ ){
-    jQuery( function( $ ){
-        $( document ).on('fwpmanip.init', function(){
-            $('.fwpmanip-select2').select2();
-        });
-    });
-})( jQuery )
--- assets/controls/slider/js/ion.rangeSlider.init.js
+++
@@ -1,7 +0,0 @@
-( function( $ ){
-    jQuery( function( $ ){
-        $( document ).on('fwpmanip.init', function(){
-            $(".fwpmanip-slider").ionRangeSlider();
-        });
-    });
-})( jQuery )
